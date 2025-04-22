4728e582440df2d573d1770ac28ce4d5e79894ea
RyanPacheco <oryanpacheco@gmail.com>
2025-04-22 03:01:58 -0300

    Acrescentando randomização de valores salariais para os candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,86 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import infodocandidato.Candidato;
+
 public class ProcessoSeletivo {
 
+	private static final double SALARIO_MAXIMO = 2000.0;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		try (Scanner sc = new Scanner(System.in)) {
+
+			List<Candidato> candidatosAprovados = new ArrayList<>();
+
+			int opcao;
+
+			do {
+
+				System.out.println("\n1. Cadastrar candidato");
+				System.out.println("2. Listar candidatos aprovados");
+				System.out.println("0. Sair");
+
+				opcao = sc.nextInt();
+				sc.nextLine();
+
+				switch (opcao) {
+
+				case 1:
+					
+					System.out.print("Nome: ");
+					String nome = sc.nextLine();
+
+					System.out.print("Idade: ");
+					int idade = sc.nextInt();
+					sc.nextLine();
+
+					System.out.print("Cargo desejado: ");
+					String cargo = sc.nextLine();
+
+					System.out.print("\nPretensão salarial (R$) ");
+					double salarioPretendido = Candidato.ValorPretendido();
+					System.out.printf("\nPretensão salario gerada: R$ %.2f", salarioPretendido);
+					
+
+					if (salarioPretendido <= SALARIO_MAXIMO) {
+
+						candidatosAprovados.add(new Candidato(nome, idade, cargo, salarioPretendido));
+						
+						System.out.println("\nAprovado");
+					}else{
+
+						System.out.println("\nRecusado");
+					}
+					
+					break;
+
+				case 2:
+					
+					System.out.println("Candidatos aprovados");
+					if (candidatosAprovados.isEmpty()) {
+
+						System.out.println("Nenhum aprovado até o momento");
+
+					}else{
+
+						for (Candidato c : candidatosAprovados) {
+							System.out.println(c);
+						}
+					}
+					
+					break;
+
+				}
+
+			} while (opcao != 0);
+
+		}
 
 	}
 
--- src/infodocandidato/Candidato.java
+++ src/infodocandidato/Candidato.java
@@ -1,5 +1,32 @@
 package infodocandidato;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Candidato {
 
+	String nome;
+	int idade;
+	String cargo;
+	double salarioPretendido = ValorPretendido(); 
+
+		public Candidato(String nome, int idade, String cargo, double salarioPretendido) {
+
+		this.nome = nome;
+		this.idade = idade;
+		this.cargo = cargo;
+		this.salarioPretendido = salarioPretendido;
+	}
+
+		public String toSring() {
+			return "Nome: " + nome + 
+				   "Idade: " + idade +
+				   "Cargo" + cargo + 
+				   "Pretensão salarial: R$ %.2f " + salarioPretendido; 
+			
+		}
+		
+	public static double ValorPretendido () {
+		
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);		
+	}
 }
